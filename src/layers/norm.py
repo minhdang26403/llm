952ba21d490643026b419502a71699449de8646c
Dang Truong <truongminhdang26403@gmail.com>
2026-03-09 15:54:31 -0400

    feat: implement Root Mean Square Layer Normalization

--- src/layers/norm.py
+++ src/layers/norm.py
@@ -24,3 +24,26 @@ class LayerNorm(nn.Module):
 
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
         return self.forward(x)
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, normalized_shape: int | list[int], eps=None):
+        super().__init__()
+
+        if isinstance(normalized_shape, int):
+            shape = (normalized_shape,)
+        else:
+            shape = tuple(normalized_shape)  # type: ignore
+
+        self.normalized_dim = tuple(range(-len(shape), 0))
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(shape))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        eps = self.eps if self.eps else torch.finfo(x.dtype).eps
+        rms = torch.sqrt(eps + torch.mean(x**2, dim=self.normalized_dim, keepdim=True))
+
+        return x / rms * self.weight
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        return self.forward(x)
--- tests/norm_test.py
+++ tests/norm_test.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from layers.norm import LayerNorm
+from layers.norm import LayerNorm, RMSNorm
 
 
 def test_layer_norm_single_dim():
@@ -28,6 +28,35 @@ def test_layer_norm_multiple_dims():
     output = layer_norm(input)
 
     torch_layer_norm = nn.LayerNorm([C, H, W])
+    expected = torch_layer_norm(input)
+
+    assert torch.allclose(output, expected, atol=1e-6)
+
+
+def test_rms_norm_single_dim():
+    # NLP Example
+    batch, sentence_length, embedding_dim = 20, 5, 10
+    embedding = torch.randn(batch, sentence_length, embedding_dim)
+
+    layer_norm = RMSNorm(embedding_dim)
+    output = layer_norm(embedding)
+
+    torch_layer_norm = nn.RMSNorm(embedding_dim)
+    expected = torch_layer_norm(embedding)
+
+    assert torch.allclose(output, expected, atol=1e-6)
+
+
+def test_rms_norm_multiple_dims():
+    # Image Example
+    N, C, H, W = 20, 5, 10, 10
+    input = torch.randn(N, C, H, W)
+    # Normalize over the last three dimensions (i.e. the channel and spatial dimensions)
+    # as shown in the image below
+    layer_norm = RMSNorm([C, H, W])
+    output = layer_norm(input)
+
+    torch_layer_norm = nn.RMSNorm([C, H, W])
     expected = torch_layer_norm(input)
 
     assert torch.allclose(output, expected, atol=1e-6)
