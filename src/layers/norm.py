e6bdbef1c68d43b0b9d9e8f8c05345ed6da4d11f
Dang Truong <truongminhdang26403@gmail.com>
2026-03-09 15:53:03 -0400

    feat: implement Layer Normalization

---
+++ src/layers/norm.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, normalized_shape: int | list[int], eps=1e-05):
+        super().__init__()  # CRITICAL: Always call super()
+
+        if isinstance(normalized_shape, int):
+            shape = (normalized_shape,)
+        else:
+            shape = tuple(normalized_shape)  # type: ignore
+
+        self.normalized_dim = tuple(range(-len(shape), 0))
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(shape))
+        self.bias = nn.Parameter(torch.zeros(shape))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        mean = x.mean(dim=self.normalized_dim, keepdim=True)
+        var = x.var(dim=self.normalized_dim, correction=0, keepdim=True)
+
+        return (x - mean) / torch.sqrt(var + self.eps) * self.weight + self.bias
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        return self.forward(x)
---
+++ tests/norm_test.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+from layers.norm import LayerNorm
+
+
+def test_layer_norm_single_dim():
+    # NLP Example
+    batch, sentence_length, embedding_dim = 20, 5, 10
+    embedding = torch.randn(batch, sentence_length, embedding_dim)
+
+    layer_norm = LayerNorm(embedding_dim)
+    output = layer_norm(embedding)
+
+    torch_layer_norm = nn.LayerNorm(embedding_dim)
+    expected = torch_layer_norm(embedding)
+
+    assert torch.allclose(output, expected, atol=1e-6)
+
+
+def test_layer_norm_multiple_dims():
+    # Image Example
+    N, C, H, W = 20, 5, 10, 10
+    input = torch.randn(N, C, H, W)
+    # Normalize over the last three dimensions (i.e. the channel and spatial dimensions)
+    # as shown in the image below
+    layer_norm = LayerNorm([C, H, W])
+    output = layer_norm(input)
+
+    torch_layer_norm = nn.LayerNorm([C, H, W])
+    expected = torch_layer_norm(input)
+
+    assert torch.allclose(output, expected, atol=1e-6)
