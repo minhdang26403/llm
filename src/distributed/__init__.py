ab75a34b6e6ebc13cb39c2255510659713233d77
Dang Truong <truongminhdang26403@gmail.com>
2026-03-23 10:33:43 -0400

    chore: update module export

--- src/distributed/__init__.py
+++ src/distributed/__init__.py
@@ -1,5 +1,8 @@
-from .distributed_data_parallel import DistributedDataParallel
+from .distributed_data_parallel import (
+    DistributedDataParallel,
+    DistributedDataParallelStaticBucket,
+)
 
-__all__ = ["DistributedDataParallel"]
+__all__ = ["DistributedDataParallel", "DistributedDataParallelStaticBucket"]
 
 assert sorted(__all__) == __all__
